cf1b774f161a47d2570a98ac90d9adc7ea91dc51
Your Name <you@example.com>
2024-04-06 08:20:57 +0000

    api improvements

--- api.py
+++ api.py
@@ -15,6 +15,24 @@ class AddBotToCacheServer(BaseModel):
     invite_code: str
     added: bool
 
+@app.get("/getCacheServerOfBot")
+async def get_cache_server_of_bot(request: Request, bot_id: str):
+    """Returns the cache server of a bot"""
+    cache_server = await bot.pool.fetchval("SELECT guild_id FROM cache_server_bots WHERE bot_id = $1", bot_id)
+
+    if cache_server is None:
+        raise HTTPException(status_code=404, detail="Bot not found in any cache server")
+    
+    data = await bot.pool.fetchrow("SELECT invite_code FROM cache_servers WHERE guild_id = $1", cache_server)
+    
+    guild = bot.get_guild(int(cache_server))
+    if guild is None:
+        raise HTTPException(status_code=500, detail="Cache server not found despite existing in database")
+
+    member = guild.get_member(int(bot_id))
+
+    return {"guild_id": cache_server, "invite_code": data["invite_code"], "member": member is not None}
+
 @app.post("/addBotToCacheServer", response_model=AddBotToCacheServer)
 async def add_bot_to_cache_server(request: Request, bot_id: str):
     """Adds a bot to a cache server. Internal-only"""
